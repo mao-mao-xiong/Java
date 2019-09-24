d4db99105bc2ebc0f140ec273f63b2b8f1ebd74a
mao-mao-xiong <55074568+mao-mao-xiong@users.noreply.github.com>
2019-09-24 23:07:53 +0800

    Add files via upload

--- Example15.java
+++ Example15.java
@@ -1,14 +1,32 @@
-class Single {
-  private static Single INSTANCE =new Single();
-  private Single () {}
-  public static Single getInstance() {
-  return INSTANCE;
-  }
-}
-class Example15{
-  public static void main (String[] args){
-   Single s1 = Single.getInstance();
-   Single s2 = Single.getInstance();
-   System.out.println(s1==s2);
+interface Animal {
+  void shout ();
  }
+class Cat implements Animal{
+  public void shout () {
+   System.out.println("喵喵······");
+ }
+  void sleep () {
+  System.out.println("猫睡觉······");
+ } 
+}
+class Dog implements Animal {
+  public void shout () {
+      System.out.println("汪汪······");
+ }
+}
+public class Example15{
+   public  static  void  main (String [] args){
+   Dog dog=new Dog();
+   animalShout(dog);
+  }
+  public  static void  animalShout (Animal animal){
+      if (animal instanceof Cat){
+     Cat cat =(Cat)  animal;
+    cat.shout();
+     cat.sleep();
+  }
+   else{
+    System.out.println("this animal is not a cat");
+ }
+}
 }
